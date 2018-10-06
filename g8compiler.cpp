cd1761c309785f0f49306ccb53508d0476e5b2f2
racaljk <1948638989@qq.com>
2018-10-06 18:44:24 +0800

    lexer: parse float decimal

--- g8compiler.cpp
+++ g8compiler.cpp
@@ -1,7 +1,7 @@
 //===----------------------------------------------------------------------===//
 // golang specification can be found here: https://golang.org/ref/spe
-// 
-// in development mode, i consider raise exception when met error , they will be 
+//
+// in development mode, i consider raise exception when met error , they will be
 // replaced with error stream later
 //===----------------------------------------------------------------------===//
 
@@ -9,11 +9,11 @@
 #include <array>
 #include <cctype>
 #include <cstdio>
+#include <exception>
 #include <fstream>
 #include <functional>
 #include <string>
 #include <vector>
-#include <exception>
 
 using namespace std;
 
@@ -23,21 +23,93 @@ string keywords[] = {"break",    "default",     "func",   "interface", "select",
                      "const",    "fallthrough", "if",     "range",     "type",
                      "continue", "for",         "import", "return",    "var"};
 
-enum Token{
-    KW_break,KW_default,KW_func,KW_interface,KW_select,KW_case,KW_defer,KW_go,KW_map,KW_struct,KW_chan,KW_else,KW_goto,KW_package,KW_switch,
-    KW_const,KW_fallthrough,KW_if,KW_range,KW_type,KW_continue,KW_for,KW_import,KW_return,KW_var,OP_ADD,OP_BITAND,OP_ADDASSIGN,P_BITANDASSIGN,
-    OP_AND,OP_EQ,OP_NE,OP_LPAREN,OP_RPAREN,OP_SUB,OP_BITOR,OP_SUBASSIGN,OP_BITORASSIGN,OP_OR,OP_LT,OP_LE,OP_LBRACKET,OP_RBRACKET,OP_MUL,OP_XOR,
-    OP_MULASSIGN,OP_XORASSIGN,OP_CHAN,OP_GT,OP_GE,OP_LBRACK,OP_RBRACE,OP_DIV,OP_LSHIFT,OP_DIVASSIGN,OP_LSFTASSIGN,OP_INC,OP_ASSIGN,OP_SHORTASSIGN,
-    OP_COMMA,OP_SEMI,OP_MOD,OP_RSHIFT,OP_MODASSIGN,OP_RSFTASSIGN,OP_DEC,OP_NOT,OP_VARIADIC,OP_DOT,OP_COLON,OP_ANDNOT,OP_ANDNOTASSIGN,TK_ID,
-    LITERAL_INT,LITERAL_FLOAT,LITERAL_IMG,LITERAL_RUNE,LITERAL_STR};
-
+enum Token {
+    KW_break,
+    KW_default,
+    KW_func,
+    KW_interface,
+    KW_select,
+    KW_case,
+    KW_defer,
+    KW_go,
+    KW_map,
+    KW_struct,
+    KW_chan,
+    KW_else,
+    KW_goto,
+    KW_package,
+    KW_switch,
+    KW_const,
+    KW_fallthrough,
+    KW_if,
+    KW_range,
+    KW_type,
+    KW_continue,
+    KW_for,
+    KW_import,
+    KW_return,
+    KW_var,
+    OP_ADD,
+    OP_BITAND,
+    OP_ADDASSIGN,
+    P_BITANDASSIGN,
+    OP_AND,
+    OP_EQ,
+    OP_NE,
+    OP_LPAREN,
+    OP_RPAREN,
+    OP_SUB,
+    OP_BITOR,
+    OP_SUBASSIGN,
+    OP_BITORASSIGN,
+    OP_OR,
+    OP_LT,
+    OP_LE,
+    OP_LBRACKET,
+    OP_RBRACKET,
+    OP_MUL,
+    OP_XOR,
+    OP_MULASSIGN,
+    OP_XORASSIGN,
+    OP_CHAN,
+    OP_GT,
+    OP_GE,
+    OP_LBRACK,
+    OP_RBRACE,
+    OP_DIV,
+    OP_LSHIFT,
+    OP_DIVASSIGN,
+    OP_LSFTASSIGN,
+    OP_INC,
+    OP_ASSIGN,
+    OP_SHORTASSIGN,
+    OP_COMMA,
+    OP_SEMI,
+    OP_MOD,
+    OP_RSHIFT,
+    OP_MODASSIGN,
+    OP_RSFTASSIGN,
+    OP_DEC,
+    OP_NOT,
+    OP_VARIADIC,
+    OP_DOT,
+    OP_COLON,
+    OP_ANDNOT,
+    OP_ANDNOTASSIGN,
+    TK_ID,
+    LITERAL_INT,
+    LITERAL_FLOAT,
+    LITERAL_IMG,
+    LITERAL_RUNE,
+    LITERAL_STR
+};
 
 int line = 1, column = 1;
 
 tuple<Token, string> next(fstream& f) {
     char c = f.peek();
     auto isdelimiter = [](char c) {
-        return c == ' ' || c == '\n' || c == '\t' || c == '\r';
+        return c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == EOF;
     };
 
     for (; c == ' ' || c == '\r' || c == '\t'; column++) {
@@ -54,7 +126,7 @@ tuple<Token, string> next(fstream& f) {
     if (isalpha(c) || c == '_') {
         string tempIdent;
         while (!isdelimiter(c)) {
-            if(isalnum(c) || c == '_') {
+            if (isalnum(c) || c == '_') {
                 tempIdent += c;
                 c == '\n' ? (line += 1, column = 0) : (column++);
                 f.get();
@@ -63,7 +135,6 @@ tuple<Token, string> next(fstream& f) {
                 throw exception("invalid identifier name");
             }
         }
-
 
         for (int i = 0; i < sizeof(keywords); i++)
             if (keywords[i] == tempIdent) {
@@ -78,6 +149,14 @@ tuple<Token, string> next(fstream& f) {
     // octal_lit   = "0" { octal_digit } .
     // hex_lit     = "0" ( "x" | "X" ) hex_digit { hex_digit } .
 
+    // float_lit = decimals "." [ decimals ] [ exponent ] |
+    //         decimals exponent |
+    //         "." decimals [ exponent ] .
+    // decimals  = decimal_digit { decimal_digit } .
+    // exponent  = ( "e" | "E" ) [ "+" | "-" ] decimals .
+
+    // imaginary_lit = (decimals | float_lit) "i" .
+
     if (isdigit(c)) {
         string tempDigit;
         // int_lit = octal_lit | hex_lit
@@ -86,7 +165,6 @@ tuple<Token, string> next(fstream& f) {
             column++;
             tempDigit += c;
             c = f.peek();
-            // hex_lit     = "0" ( "x" | "X" ) hex_digit { hex_digit } .
             if (c == 'x' || c == 'X') {
                 f.get();
                 column++;
@@ -104,47 +182,107 @@ tuple<Token, string> next(fstream& f) {
                     }
                 }
                 return make_tuple(LITERAL_INT, tempDigit);
-            }
-            // octal_lit   = "0" { octal_digit } .
-            else if (c >= '0' && c <= '7') {
+            } else if (c >= '0' && c <= '7') {
                 f.get();
                 column++;
                 tempDigit += c;
                 c = f.peek();
-                while (c >= '0' && c <= '7') {
+                while (!isdelimiter(c)) {
+                    if (c >= '0' && c <= '7') {
+                        f.get();
+                        column++;
+                        tempDigit += c;
+                        c = f.peek();
+                    } else if (c == '8' || c == '9' || c=='.' || c=='e'||c=='E') {
+                        goto shall_float;
+                    } else {
+                        throw exception("invalid hexadecimal or octal number");
+                    }
+                }
+                return make_tuple(LITERAL_INT, tempDigit);
+            }
+            goto shall_float;
+        } else {
+            f.get();
+            column++;
+            tempDigit += c;
+            c = f.peek();
+        shall_float:  // skip char consuming and appending since we did that
+                      // before jumping here;
+            bool hasDot = false, hasExponent = false;
+            while (!isdelimiter(c)) {
+                if (c >= '0' && c <= '9') {
+                    f.get();
+                    column++;
+                    tempDigit += c;
+                    c = f.peek();
+                } else if (c == '.' && !hasDot) {
+                    f.get();
+                    column++;
+                    tempDigit += c;
+                    c = f.peek();
+                } else if ((c == 'e' && !hasExponent) ||(c == 'E' && !hasExponent)) {
+                    hasExponent = true;
+                    f.get();
+                    column++;
+                    tempDigit += c;
+                    c = f.peek();
+                    if (c == '+' || c == '-') {
+                        f.get();
+                        column++;
+                        tempDigit += c;
+                        c = f.peek();
+                    } 
+                } else {
+                    throw exception("invalid integer number");
+                }
+            }
+            return make_tuple(LITERAL_INT, tempDigit);
+        }
+    } else if (c == '.') {
+        string tempDigit;
+        f.get();
+        column++;
+        tempDigit += c;
+        c = f.peek();
+        bool hasDot = false, hasExponent = false;
+        while (!isdelimiter(c)) {
+            if (c >= '0' && c <= '9') {
+                f.get();
+                column++;
+                tempDigit += c;
+                c = f.peek();
+            } else if (c == '.' && !hasDot) {
+                f.get();
+                column++;
+                tempDigit += c;
+                c = f.peek();
+            } else if ((c == 'e' && !hasExponent) || (c == 'E' && !hasExponent)) {
+                hasExponent = true;
+                f.get();
+                column++;
+                tempDigit += c;
+                c = f.peek();
+                if (c == '+' || c == '-') {
                     f.get();
                     column++;
                     tempDigit += c;
                     c = f.peek();
                 }
-                return make_tuple(LITERAL_INT, tempDigit);
             } else {
-                throw exception("invalid hexadecimal or octal number");
+                throw exception("invalid integer number");
             }
+            
         }
-        // int_lit = decimal_lit
-        else if (c >= '1' && c <= '9') {
-            f.get();
-            column++;
-            tempDigit += c;
-            c = f.peek();
-            while (c >= '0' && c <= '9') {
-                f.get();
-                column++;
-                tempDigit += c;
-                c = f.peek();
-            }
-            return make_tuple(LITERAL_INT, tempDigit);
-        }
+        return make_tuple(LITERAL_INT, tempDigit);
     }
 }
-
-
 
 void tokenize(fstream& f) {
     while (f.good()) {
         auto [token, lexeme] = next(f);
-        fprintf(stdout, "<%d,%s,%d,%d>\n", token, lexeme.c_str(), line, column);
+        fprintf(stdout, "<%d,\"%s\",%d,%d>\n", token, lexeme.c_str(), line,
+                column);
     }
 }
 
